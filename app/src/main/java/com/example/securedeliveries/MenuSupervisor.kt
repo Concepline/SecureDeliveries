45d70aac107dbfa77ab9db2c31a31427d42ac05a
Concepline <concepcionanaline.ec@gmail.com>
2023-05-16 14:53:56 -0600

    Se agrego la función cerrar sesión para el usuario supervisor.

--- app/src/main/java/com/example/securedeliveries/MenuSupervisor.kt
+++ app/src/main/java/com/example/securedeliveries/MenuSupervisor.kt
@@ -1,11 +1,23 @@
 package com.example.securedeliveries
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MenuSupervisor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_supervisor)
+        cerrarSesion()
+    }
+
+    private fun cerrarSesion() {
+        val btnCerrar = findViewById<ImageView>(R.id.btn_cerrar_sesion_supervisor)
+
+        btnCerrar.setOnClickListener {
+            startActivity(Intent(this@MenuSupervisor,LoginSupervisor::class.java))
+            finish()
+        }
     }
 }
