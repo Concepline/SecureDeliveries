76d184dc34766dfead8e01716d4bbe137784832a
Concepline <concepcionanaline.ec@gmail.com>
2023-05-17 12:56:25 -0600

    Cifra y descifra imagenes tomadas de la camara.

--- app/src/main/java/com/example/securedeliveries/RepartidorFotografia.kt
+++ app/src/main/java/com/example/securedeliveries/RepartidorFotografia.kt
@@ -11,7 +11,14 @@ import android.provider.MediaStore
 import android.widget.ImageView
 import android.Manifest
 import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.widget.Button
 import android.widget.Toast
+import java.lang.Math.PI
+import java.lang.Math.sin
+import kotlin.random.Random
 
 class RepartidorFotografia : AppCompatActivity() {
 
@@ -20,6 +27,7 @@ class RepartidorFotografia : AppCompatActivity() {
     private lateinit var fotografia: ImageView
     private lateinit var cancelar: ImageView
     private lateinit var aceptar: ImageView
+    private lateinit var des: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +35,17 @@ class RepartidorFotografia : AppCompatActivity() {
         abreCamara_Click()
         otrafoto()
         resizeImage()
+        des()
         cancelar()
     }
 
+    fun des()
+    {
+        des = findViewById(R.id.btn_des)
+        des.setOnClickListener {
+            descifrar()
+        }
+    }
     fun resizeImage() {
         aceptar = findViewById(R.id.btn_aceptar)
         aceptar.setOnClickListener {
@@ -46,7 +62,93 @@ class RepartidorFotografia : AppCompatActivity() {
 // Puedes imprimir o utilizar los valores obtenidos según tus necesidades
             println("Ancho del ImageView: $width")
             println("Alto del ImageView: $height")
+            cifrar()
         }
+    }
+
+    private fun descifrar(){
+        fotografia = findViewById(R.id.img_foto)
+
+        val key1:Long = 10
+        val r = 0.99 // Parámetro de control r E (0,1]
+        val k = 10 // Número de iteraciones del mapa caótico (Mínimo 10 iteraciones más aleatorio)
+
+        val drawable = fotografia.drawable as BitmapDrawable
+        val bitmap =drawable.bitmap
+
+        val pixels = getPixels(bitmap)
+        val sortedPixels = decryptImage(pixels,key1,r, k)
+        val decryptedBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        decryptedBitmap.setPixels(sortedPixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        fotografia.setImageBitmap(decryptedBitmap)
+        println("terminno descifrado")
+    }
+
+
+    private fun decryptImage(sortedPixels: IntArray, seed: Long, r: Double, k: Int): IntArray {
+        val nPixels = sortedPixels.size
+
+        val random = Random(seed)
+        val Xo = DoubleArray(nPixels) { random.nextDouble() }
+        val Xn = DoubleArray(nPixels)
+        val decryptedPixels = IntArray(nPixels)
+
+        for (i in 0 until k) {
+            for (j in 0 until nPixels) {
+                Xn[j] = r * sin(PI * Xo[j])
+            }
+            Xo.indices.sortedBy { Xn[it] }.forEachIndexed { index, sortedIndex ->
+                decryptedPixels[sortedIndex] = sortedPixels[index]
+            }
+            Xn.copyInto(Xo)
+        }
+
+        return decryptedPixels
+    }
+    private fun cifrar(){
+        fotografia = findViewById(R.id.img_foto)
+
+        val key1:Long = 10
+        val r = 0.99 // Parámetro de control r E (0,1]
+        val k = 10 // Número de iteraciones del mapa caótico (Mínimo 10 iteraciones más aleatorio)
+
+        val drawable = fotografia.drawable as BitmapDrawable
+        val bitmap =drawable.bitmap
+
+        val pixels = getPixels(bitmap)
+        val sortedPixels = encryptImage(pixels,key1, r, k)
+        val encryptedBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        encryptedBitmap.setPixels(sortedPixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        fotografia.setImageBitmap(encryptedBitmap)
+    }
+
+    private fun getPixels(bitmap: Bitmap): IntArray {
+        val width = bitmap.width
+        val height = bitmap.height
+        val pixels = IntArray(width * height)
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+        return pixels
+    }
+
+    private fun encryptImage(pixels: IntArray, seed: Long, r: Double, k: Int): IntArray {
+        val nPixels = pixels.size
+
+        val random = Random(seed)
+        val Xo = DoubleArray(nPixels) { random.nextDouble() }
+        val Xn = DoubleArray(nPixels)
+        val sortedPixels = IntArray(nPixels)
+
+        for (i in 0 until k) {
+            for (j in 0 until nPixels) {
+                Xn[j] = r * kotlin.math.sin(PI * Xo[j])
+            }
+            Xo.indices.sortedBy { Xn[it] }.forEachIndexed { index, sortedIndex ->
+                sortedPixels[index] = pixels[sortedIndex]
+            }
+            Xn.copyInto(Xo)
+        }
+
+        return sortedPixels
     }
 
     private fun cancelar() {
